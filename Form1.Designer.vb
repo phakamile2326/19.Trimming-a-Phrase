e262e5b434b8420535965aa00dba9cc4e8f42dc0
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 03:02:31 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnOk = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnOk
+        ' 
+        btnOk.Location = New Point(269, 164)
+        btnOk.Name = "btnOk"
+        btnOk.Size = New Size(75, 23)
+        btnOk.TabIndex = 0
+        btnOk.Text = "Ok"
+        btnOk.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(192, 94)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(59, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "MyPhrase"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(383, 86)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(292, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(btnOk)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnOk As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackColor = Color.Azure
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Dim MyPhrase As String
+        MyPhrase = InputBox("Enter Your Phrase")
+        TextBox1.Text = Trim(MyPhrase)
+    End Sub
+End Class
